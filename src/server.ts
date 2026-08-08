2ab0dc912ced93bcd1c4512117c96963a3cd2abd
Abul Bashar <bashar35790@gmail.com>
2026-08-08 18:41:42 +0600

    added app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config();
+import app from "./app";
 
-const app=express();
-app.use(cors());
-app.use(express.json());
-app.listen(process.env.PORT||5000);
+const startServer = async () => {
+    try {
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 5000}`);
+        });
+    } catch (error) {
+        console.error("Error starting server:", error);
+    }
+};
+
+startServer();  
